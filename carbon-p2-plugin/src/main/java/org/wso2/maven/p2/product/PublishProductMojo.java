08f29d88e5b23c0e1eba411e34318d48d30797e1
dinushab <dinushaboteju@gmail.com>
2015-10-08 11:40:53 +0530

    CARBON-15482: Moved the PublishProductMojo to new package.

--- carbon-p2-plugin/src/main/java/org/wso2/maven/p2/product/PublishProductMojo.java
+++ carbon-p2-plugin/src/main/java/org/wso2/maven/p2/product/PublishProductMojo.java
@@ -15,7 +15,7 @@
 * specific language governing permissions and limitations
 * under the License.
 */
-package org.wso2.maven.p2;
+package org.wso2.maven.p2.product;
 
 
 import org.apache.maven.plugin.AbstractMojo;
