e13fc7be8de1f3b5e6e2051f32c642e62ee38119
dinushab <dinushaboteju@gmail.com>
2015-10-08 10:11:12 +0530

    CARBON-15481: Introduced java 5 annotations to GenerateProfileMojo.java

--- carbon-p2-plugin/src/main/java/org/wso2/maven/p2/profile/GenerateProfileMojo.java
+++ carbon-p2-plugin/src/main/java/org/wso2/maven/p2/profile/GenerateProfileMojo.java
@@ -20,6 +20,9 @@ package org.wso2.maven.p2.profile;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.eclipse.tycho.model.ProductConfiguration;
 import org.eclipse.tycho.p2.facade.internal.P2ApplicationLauncher;
@@ -33,41 +36,35 @@ import java.net.URL;
 import java.util.Properties;
 
 
-
-
-/**
- * @goal materialize-product
- */
+@Mojo(name = "generate-profile")
 public class GenerateProfileMojo extends AbstractMojo {
-    /**
-     * @parameter expression="${project}"
-     * @required
-     */
+
+    @Parameter(required = true, defaultValue = "${project}")
     protected MavenProject project;
     /**
      * Metadata repository name
-     *     @parameter
      */
+    @Parameter
     private URL metadataRepository;
     /**
      * Artifact repository name
-     *      @parameter
      */
+    @Parameter
     private URL artifactRepository;
-
-
 
     /**
      * The product configuration, a .product file. This file manages all aspects
      * of a product definition from its constituent plug-ins to configuration
      * files to branding.
-     *
-     * @parameter expression="${productConfiguration}"
      */
+    @Parameter(defaultValue = "${productConfiguration}")
     private File productConfigurationFile;
 
-    /** @parameter */
+    /**
+     * @parameter
+     */
     private URL targetPath;
+
     /**
      * Parsed product configuration file
      */
@@ -76,70 +73,62 @@ public class GenerateProfileMojo extends AbstractMojo {
     /**
      * The new profile to be created during p2 Director install &
      * the default profile for the the application which is set in config.ini
-     *
-     * @parameter expression="${profile}"
      */
+    @Parameter(defaultValue = "${profile}")
     private String profile;
 
 
-    /** @component */
+    @Component
     private P2ApplicationLauncher launcher;
 
     /**
      * Kill the forked test process after a certain number of seconds. If set to 0, wait forever for
      * the process, never timing out.
-     *
-     * @parameter expression="${p2.timeout}"
      */
+    @Parameter(defaultValue = "${p2.timeout}")
     private int forkedProcessTimeoutInSeconds;
 
     public void execute() throws MojoExecutionException, MojoFailureException {
         try {
-            if (profile == null){
+            if (profile == null) {
                 profile = P2Constants.DEFAULT_PROFILE_ID;
             }
             deployRepository();
             //updating profile's config.ini p2.data.area property using relative path
             File profileConfigIni = FileManagementUtil.getProfileConfigIniFile(targetPath.getPath(), profile);
             FileManagementUtil.changeConfigIniProperty(profileConfigIni, "eclipse.p2.data.area", "@config.dir/../../p2/");
-        }catch (Exception e) {
-            throw new MojoExecutionException("Cannot generate P2 metadata", e);
+        } catch (IOException e) {
+            throw new MojoExecutionException(e.getMessage(), e);
         }
     }
 
     private void regenerateCUs()
-            throws MojoExecutionException, MojoFailureException
-    {
-        getLog().debug( "Regenerating config.ini" );
+            throws MojoExecutionException, MojoFailureException {
+        getLog().debug("Regenerating config.ini");
         Properties props = new Properties();
         String id = productConfiguration.getId();
 
         setPropertyIfNotNull(props, "osgi.bundles", getFeaturesOsgiBundles());
-        setPropertyIfNotNull( props, "osgi.bundles.defaultStartLevel", "4" );
-        if(profile == null){
+        setPropertyIfNotNull(props, "osgi.bundles.defaultStartLevel", "4");
+        if (profile == null) {
             profile = "profile";
         }
-        setPropertyIfNotNull( props, "eclipse.p2.profile", profile);
-        setPropertyIfNotNull( props, "eclipse.product", id );
-        setPropertyIfNotNull( props, "eclipse.p2.data.area", "@config.dir/../p2/");
-        setPropertyIfNotNull( props, "eclipse.application", productConfiguration.getApplication() );
+        setPropertyIfNotNull(props, "eclipse.p2.profile", profile);
+        setPropertyIfNotNull(props, "eclipse.product", id);
+        setPropertyIfNotNull(props, "eclipse.p2.data.area", "@config.dir/../p2/");
+        setPropertyIfNotNull(props, "eclipse.application", productConfiguration.getApplication());
 
 
-
-
-        File configsFolder = new File( targetPath.toString(), "configuration" );
+        File configsFolder = new File(targetPath.toString(), "configuration");
         configsFolder.mkdirs();
 
-        File configIni = new File( configsFolder, "config.ini" );
-        try
-        {
-            FileOutputStream fos = new FileOutputStream( configIni );
-            props.store( fos, "Product Runtime Configuration File" );
+        File configIni = new File(configsFolder, "config.ini");
+        try {
+            FileOutputStream fos = new FileOutputStream(configIni);
+            props.store(fos, "Product Runtime Configuration File");
             fos.close();
-        }
-        catch ( IOException e )
-        {
-            throw new MojoExecutionException( "Error creating .eclipseproduct file.", e );
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error creating .eclipseproduct file.", e);
         }
 
     }
@@ -148,12 +137,12 @@ public class GenerateProfileMojo extends AbstractMojo {
         String bundles = "org.eclipse.equinox.common@2:start," +
                 "org.eclipse.update.configurator@3:start," +
                 "org.eclipse.core.runtime@start,org.eclipse.equinox.ds@1:start," +
-                "org.eclipse.equinox.simpleconfigurator@1:start" ;
+                "org.eclipse.equinox.simpleconfigurator@1:start";
         return bundles;
     }
 
-    private void deployRepository() throws Exception{
-        productConfiguration = ProductConfiguration.read( productConfigurationFile );
+    private void deployRepository() throws MojoFailureException, IOException {
+        productConfiguration = ProductConfiguration.read(productConfigurationFile);
         P2ApplicationLauncher launcher = this.launcher;
 
         launcher.setWorkingDirectory(project.getBasedir());
@@ -162,12 +151,12 @@ public class GenerateProfileMojo extends AbstractMojo {
         launcher.addArguments(
                 "-metadataRepository", metadataRepository.toExternalForm(),
                 "-artifactRepository", metadataRepository.toExternalForm(),
-                "-installIU",productConfiguration.getId(),
+                "-installIU", productConfiguration.getId(),
                 "-profileProperties", "org.eclipse.update.install.features=true",
-                "-profile",profile.toString(),
-                "-bundlepool",targetPath.toExternalForm(),
+                "-profile", profile.toString(),
+                "-bundlepool", targetPath.toExternalForm(),
                 //to support shared installation in carbon
-                "-shared" , targetPath.toExternalForm() + File.separator + "p2",
+                "-shared", targetPath.toExternalForm() + File.separator + "p2",
                 //target is set to a separate directory per Profile
                 "-destination", targetPath.toExternalForm() + File.separator + profile,
                 "-p2.os", "linux",
@@ -181,15 +170,11 @@ public class GenerateProfileMojo extends AbstractMojo {
         if (result != 0) {
             throw new MojoFailureException("P2 publisher return code was " + result);
         }
-
     }
 
-    private void setPropertyIfNotNull( Properties properties, String key, String value )
-    {
-        if ( value != null )
-        {
-            properties.setProperty( key, value );
+    private void setPropertyIfNotNull(Properties properties, String key, String value) {
+        if (value != null) {
+            properties.setProperty(key, value);
         }
     }
-
 }
