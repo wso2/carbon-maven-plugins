dab28363c079abfc8ab0d44dcfa34a2770adda7f
dinushab <dinushaboteju@gmail.com>
2015-10-16 15:14:03 +0530

    CARBON-15848: Fixing review findings.

--- carbon-p2-plugin/src/main/java/org/wso2/maven/p2/utils/PropertyReplacer.java
+++ carbon-p2-plugin/src/main/java/org/wso2/maven/p2/utils/PropertyReplacer.java
@@ -1,17 +1,17 @@
 /*
- * Copyright 2004,2005 The Apache Software Foundation.
+ *  Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package org.wso2.maven.p2.utils;
 
@@ -27,6 +27,14 @@ public class PropertyReplacer {
         NORMAL, SEEN_DOLLAR, WITHIN_BRACKET
     }
 
+    /**
+     * When a property is encountered in ${variable} form, traverse through the given Properties collection and
+     * update it to the actual value.
+     *
+     * @param origString String key in the form of ${variable}
+     * @param props Properties object
+     * @return actual String value represented by the orgString
+     */
     public static String replaceProperties(final String origString, final Properties props) {
 
         final char[] chars = origString.toCharArray();
