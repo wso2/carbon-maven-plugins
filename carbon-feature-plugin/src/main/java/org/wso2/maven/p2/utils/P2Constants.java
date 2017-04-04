5b2810c605fbb419fff6b38d8f1c5366c41cb50f
Kishanthan <kshanth2101@gmail.com>
2017-04-04 16:11:29 +0530

    Update runtime feature name to osgi runtime feature

--- carbon-feature-plugin/src/main/java/org/wso2/maven/p2/utils/P2Constants.java
+++ carbon-feature-plugin/src/main/java/org/wso2/maven/p2/utils/P2Constants.java
@@ -66,7 +66,7 @@ public class P2Constants {
             public static final String UID = "carbon.product.id";
             public static final String ID = "carbon.product";
             public static final String APPLICATION = "carbon.application";
-            public static final String RUNTIME_FEATURE = "org.wso2.carbon.runtime";
+            public static final String OSGI_RUNTIME_FEATURE = "org.wso2.carbon.osgi";
             public static final Boolean USE_FEATURES = false;
             public static final Boolean INCLUDE_LAUNCHERS = true;
             public static final String CONFIG_INI_USE = "default";
